156a207f74aefd2ee556c875cd78e63956fc288c
Pamela Laitano <laitanop@yahoo.com>
2018-11-08 08:53:58 -0600

    adding contact

--- components/Contacto/ContactForm.js
+++ components/Contacto/ContactForm.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
+
 import CardMedia from '@material-ui/core/CardMedia'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
 import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import Tooltip from '@material-ui/core/Tooltip'
 
 const styles = {
   root: {
@@ -22,7 +21,7 @@ const styles = {
     padding: '50px'
   },
   media: {
-    height: 240
+    height: 440
   },
   button: {
     backgroundColor: '#6A772A'
@@ -30,85 +29,70 @@ const styles = {
 }
 
 class ContactForm extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      name: '',
-      email: '',
-      message: ''
-    }
-    this.handleChange = this.handleChange.bind(this)
-  }
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
   render () {
     const { classes } = this.props
     return (
       <div className={classes.root}>
-        <Paper className={classes.card}>
-          <div>
-            <CardMedia
-              className={classes.media}
-              image='/static/imgcontact.jpg'
-              title='Contemplative Reptile'
-            />
-            <div>
-              <Typography gutterBottom variant='h5' component='h2'>
-                CCI Galaad
-              </Typography>
-              <Typography component='p'>
-                Ingresa tu nombre y correo electronico para consultas.
-              </Typography>
-              <form className={classes.container} autoComplete='off'>
-                <TextField
-                  id='filled-name'
-                  label='Nombre'
-                  className={classes.textField}
-                  name='name'
-                  onChange={this.handleChange}
-                  margin='normal'
-                  variant='filled'
-                />
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
 
-                <TextField
-                  id='filled-email'
-                  label='Correo'
-                  className={classes.textField}
-                  name='email'
-                  onChange={this.handleChange}
-                  margin='normal'
-                  variant='filled'
-                />
+              <CardMedia
+                className={classes.media}
+                image='/static/contactImg.png'
+                title='Contemplative Reptile'
+              />
+              <div>
 
-                <div>
-                  {' '}
-                  <textarea
-                    name='message'
-                    onChange={this.handleChange}
-                    rows='10'
-                    cols='60'
-                  />
+                <div style={{ textAlign: 'center', fontSize: '30px' }}>
+                  Contacto{' '}
                 </div>
-
-                <div>
-                  <Button
-                    // onClick={this.handleSubmit}
-                    variant='contained'
-                    color='primary'
-                    className={classes.button}
+                <div style={{ textAlign: 'center', fontSize: '18px' }}>
+                  Dirección: Centro de retiros Galaad, concepción San Rafale de Herdia
+                </div>
+                <div style={{ textAlign: 'center', fontSize: '18px' }}>
+                  Telefono: +506 70-03-8087
+                </div>
+                <div style={{ textAlign: 'center', fontSize: '18px' }}>
+                  Correo: info@cciheredia.cr
+                </div>
+                <div style={{ textAlign: 'center', fontSize: '18px' }}>
+                  <a
+                    style={{ textDecoration: 'none' }}
+                    target='_blank'
+                    href='https://www.facebook.com/CCIGalaad/'
                   >
-                    Enviar
-                  </Button>
+
+                    <span style={{ color: '#6A772A' }}>
+                      <i className='fab fa-facebook-f' />:{' '}
+                    </span>&nbsp; &nbsp;
+                    Siguenos en facebook
+                    {' '}
+                  </a>
 
                 </div>
 
-              </form>
+              </div>
 
-            </div>
-          </div>
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper>
 
-        </Paper>
+              <iframe
+                src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.714432443968!2d-84.07077048578132!3d10.040407092823452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0ef9bd520b641%3A0xe1c9fc2d215143b2!2sCentro+Retiros+GALAAD!5e0!3m2!1sen!2scr!4v1541686218884'
+                width='100%'
+                height='450'
+                frameBorder='0'
+                style={{ border: 0 }}
+                allowFullScreen
+              />
+
+            </Paper>
+          </Grid>
+
+        </Grid>
+
       </div>
     )
   }
