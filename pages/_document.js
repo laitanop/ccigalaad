b088700f276370f158f76a71f38e342479a73476
Pamela Laitano <laitanop@yahoo.com>
2018-11-08 10:20:47 -0600

    adding favicon

--- pages/_document.js
+++ pages/_document.js
@@ -36,6 +36,7 @@ class MyDocument extends Document {
             integrity='sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU'
             crossOrigin='anonymous'
           />
+          <link rel='icon' href='../static/Artboard1.png' />
 
         </Head>
         <body
